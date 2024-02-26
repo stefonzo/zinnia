27a1383cb4728a2ca0f7b05db3a869c4a90eeb56
stefonzo <158697620+stefonzo@users.noreply.github.com>
2024-02-25 18:09:23 -0600

    Update decks.js

--- decks.js
+++ decks.js
@@ -2,5 +2,9 @@ let decks = [
   { name: 'Chapter 11'
   , pairs: [
     [ `genetic material that provides the blueprint to produce an individual's traits`, 'DNA' ],
+    ['genetic material must contain the information necessary to construct an entire organism', 'information'], 
+    ['genetic material must be accurately copied, a process known as DNA replication', 'replication'],
+    ['After it is replicated, the genetic material can be passed from parent to offspring. It also must be passed from cell to cell during the process of cell division.', 'transmission'],
+    ['Differences in the genetic material must account for the known variation within each species and among different species', 'variation'],
   ]},
 ];
